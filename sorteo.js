af0b642da6f9b0394229894c63fa19f3ef5d79dd
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-20 19:42:36 -0300

    fix delete btn

--- sorteo.js
+++ sorteo.js
@@ -97,6 +97,8 @@ function updateList(listElement, array) {
 
     p.onclick = () => {
       p.remove();
+      members.splice(index, 1);
+      tasks.splice(index, 1);
       localStorage.clear();
     };
   });
